72478179dd55c0bec5071a597f88a4de36b9f65d
Efim Poberezkin <efim@poberezkin.ru>
2015-01-25 15:52:54 +0400

    added FuelConsumptionModel

---
+++ FillingStation/DAL/Models/FuelConsumptionModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FillingStation.Core.Vehicles;
+
+namespace FillingStation.DAL.Models
+{
+    class FuelConsumptionModel
+    {
+        public Fuel fuelType { get; set; }
+        public int carPercentage { get; set; }
+        public double fillingVolume { get; set; }
+        public double price { get; set; }
+    }
+}
